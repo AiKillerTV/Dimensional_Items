d9fa1eef8e7e020a50f8f570dd496fa07d3f299b
UnwiseDeadkilla <dusty_clark1@yahoo.com>
2015-02-18 21:00:21 -0600

    New armor materials

--- src/main/java/com/unwisedeadkilla/dimensional_items/DIArmorMaterial.java
+++ src/main/java/com/unwisedeadkilla/dimensional_items/DIArmorMaterial.java
@@ -4,10 +4,14 @@ import net.minecraftforge.common.util.EnumHelper;
 
 public class DIArmorMaterial {
 	
-	public static net.minecraft.item.ItemArmor.ArmorMaterial LavaArmorMaterial = EnumHelper.addArmorMaterial("LavaArmorMaterial", 14, new int[] {3, 8, 6, 3},  10);
-	public static net.minecraft.item.ItemArmor.ArmorMaterial EnderArmorMaterial = EnumHelper.addArmorMaterial("EnderArmorMaterial", 14, new int[]{3, 8, 6, 3}, 10);
+	public static net.minecraft.item.ItemArmor.ArmorMaterial LavaArmorMaterial = EnumHelper.addArmorMaterial("LavaArmorMatearial", 35, new int[] {3, 8, 6, 3},  12);
+	public static net.minecraft.item.ItemArmor.ArmorMaterial EnderArmorMaterial = EnumHelper.addArmorMaterial("EnderArmorMterial", 35, new int[]{3, 8, 6, 3}, 12);
 	public static net.minecraft.item.ItemArmor.ArmorMaterial QuartzArmorMaterial = EnumHelper.addArmorMaterial("QuartzArmorMaterial", 14, new int[]{3, 8, 6, 3}, 10);
-	public static net.minecraft.item.ItemArmor.ArmorMaterial ObsidianArmorMaterial = EnumHelper.addArmorMaterial("ObsidianArmorMaterial", 40, new int[]{3, 8, 6, 3}, 10);
-	public static net.minecraft.item.ItemArmor.ArmorMaterial SpeedArmorMaterial = EnumHelper.addArmorMaterial("SpeedArmorMaterial", 10, new int[]{0, 0, 0, 1}, 9);
+	public static net.minecraft.item.ItemArmor.ArmorMaterial ObsidianArmorMaterial = EnumHelper.addArmorMaterial("ObsidianArmorMaterial", 40, new int[]{3, 8, 6, 3}, 5);
+	public static net.minecraft.item.ItemArmor.ArmorMaterial SpeedArmorLeatherMaterial = EnumHelper.addArmorMaterial("SpeedArmorLeatherMaterial", 5, new int[]{1, 3, 2, 1}, 7);
+	public static net.minecraft.item.ItemArmor.ArmorMaterial SpeedArmorIronMaterial = EnumHelper.addArmorMaterial("SpeedArmorIronMaterial", 15, new int[]{2, 6, 5, 2}, 4);
+	public static net.minecraft.item.ItemArmor.ArmorMaterial SpeedArmorGoldMaterial = EnumHelper.addArmorMaterial("SpeedArmorGoldMaterial", 7, new int[]{2, 5, 3, 1}, 12);
+	public static net.minecraft.item.ItemArmor.ArmorMaterial SpeedArmorDiamondMaterial = EnumHelper.addArmorMaterial("SpeedArmorDiamondMaterial", 33, new int[]{3, 8, 6, 3}, 5);
+	public static net.minecraft.item.ItemArmor.ArmorMaterial SpeedArmorObsidianMaterial = EnumHelper.addArmorMaterial("SpeedArmorObsidianMaterial", 40, new int[]{3, 8, 6, 3}, 4);
 
 }
